fa2650899dd950bc59043c7909734414f5d7e14d
Alexandr <justdealman@gmail.com>
2015-01-15 15:45:29 +0700

    catalog

---
+++ js/dynamic.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function() {
+	$('input[type="checkbox"]').uniform();
+	$('.lb .nav > ul > li > div').css({
+		'min-height': $('.lb .nav > ul').height()+'px'
+	});
+	$('.lb .nav > ul > li').hover(
+		function() {
+			$(this).children('div').stop(true,true).show();
+		},
+		function() {
+			$(this).children('div').stop(true,true).hide();
+		}
+	)
+});
